79fc503bae7d28aa0c4e639fd6abd4ac4785a3bc
ash1910 <ashraful1910@gmail.com>
2024-01-08 21:03:51 +0600

    clean

--- app/_layout.js
+++ app/_layout.js
@@ -12,19 +12,6 @@ export default function _layout() {
         headerTintColor: "white",
       }}
     >
-      <Stack.Screen
-        name="index"
-        options={{
-          headerTitle: "Home",
-          headerRight: () => <Button onPress={() => {router.push('contact')}} title="Contact" />,
-        }}
-      />
-      <Stack.Screen name="about" options={{ headerTitle: "About" }} />
-      <Stack.Screen
-        name="blog/index"
-        options={{ headerTitle: "All Blog Posts" }}
-      />
-      <Stack.Screen name="contact" options={{ headerTitle: "Contact", presentation: 'modal' }} />
       <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
     </Stack>
   );
